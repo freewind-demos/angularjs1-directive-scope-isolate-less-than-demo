6d55e5bd8077edcf5e6ad844b2e50ee29e1c4bb9
freewind <nowindlee@gmail.com>
2018-08-30 18:30:18 +0800

    demo

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-AngularJS1 Directive Scope True Demo
-====================================
+AngularJS1 Directive `scope: { myAttr: '@outAttr' }` Demo
+=========================================================
 
-`scope`
--------
+```
+scope: { myAttr: '@outAttr' }
+```
 
-- `true`: A new child scope that prototypically inherits from its parent will be created for the directive's element. If multiple directives on the same element request a new scope, only one new scope is created.
+- `scope: {}`: 表示directive独立了，不再使用外层controller中的`$scope`了
+- `outAttr`: 表示外面需要使用`out-attr="{{ something }}"`的方式向我传参数
+- `myAttr`: 表示directive里面使用`myAttr`这个名字，并且可以通过`$scope.myAttr`引用值
+- `@`: 关键点，表示“单向”改变，只能从外部到内部
+    - 如果外层改变了`something`的内容，则`myAttr`跟着变
+    - 但是directive内部改变了`myAttr`的值，`something`不会变
 
 ```
 npm install
--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const app = angular.module('app', [])
 app.controller('myController', function ($scope) {
-    $scope.message = 'hello, myController'
-    $scope.controllerName = '(myController)'
+    $scope.userName = 'angularjs'
 })
 app.directive('myDirective', function () {
     return {
-        restrict: 'A',
-        scope: true,
-        link: function (scope, element, attrs) {
-            scope.message = 'hello, myDirective'
+        restrict: 'E',
+        scope: {
+            directiveUserName: '@userName'
+        },
+        template: '<div>' +
+            '<h3>Inside myDirective</h3>' +
+            '<div>change me will change message in myDirective only</div>' +
+            '<input ng-model="directiveUserName">' +
+            '<div class="message">directiveUserName: {{ directiveUserName }}</div>' +
+            '</div>',
+        controller: function ($scope) {
+            console.log('directiveUserName: ' + $scope.directiveUserName)
         }
     }
 })
