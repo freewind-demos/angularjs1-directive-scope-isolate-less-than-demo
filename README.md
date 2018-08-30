18e940516d762cece2e2bb7fa1c6f472b65ec2f3
freewind <nowindlee@gmail.com>
2018-08-30 21:09:48 +0800

    demo

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-AngularJS1 Directive `scope: { myAttr: '@outAttr' }` Demo
+AngularJS1 Directive `scope: { myAttr: '<outAttr' }` Demo
 =========================================================
 
 ```
-scope: { myAttr: '@outAttr' }
+scope: { myAttr: '<outAttr' }
 ```
 
+跟`=outAttr`很像，不过`=outAttr`是双向绑定，而`<outAttr`只是单项绑定
+
 - `scope: {}`: 表示directive独立了，不再使用外层controller中的`$scope`了
-- `outAttr`: 表示外面需要使用`out-attr="Hi {{ something }}"`的方式向我传参数
-    - 可以自由的使用表达式
-    - 最终得到的是一个字符串，不会是其它类型
+- `outAttr`: 表示外面需要使用`out-attr="something"`的方式向我传参数
+    - `something`必须是一个变量名
+    - 因为可以传递任何类型，所以不能有表达式`{{ }}`（不像`@outAttr`）
 - `myAttr`: 表示directive里面使用`myAttr`这个名字，并且可以通过`$scope.myAttr`引用值
-- `@`: 关键点，表示“单向”改变，只能从外部到内部
+- `<`: 关键点，表示“单向绑定”
     - 如果外层改变了`something`的内容，则`myAttr`跟着变
-    - 但是directive内部改变了`myAttr`的值，`something`不会变
+    - 但是如果directive内部改变了`myAttr`的值，`something`不会变
 
 ```
 npm install
--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const app = angular.module('app', [])
 app.controller('myController', function ($scope) {
-    $scope.userName = 'angularjs'
+    $scope.userInfo = {
+        name: 'Freewind',
+        email: 'test@test.com'
+    }
 })
 app.directive('myDirective', function () {
     return {
         restrict: 'E',
         scope: {
-            directiveUserName: '@userName'
+            directiveUserInfo: '<userInfo'
         },
         template: '<div>' +
             '<h3>Inside myDirective</h3>' +
-            '<div>change me will change message in myDirective only</div>' +
-            '<input ng-model="directiveUserName">' +
-            '<div class="message">directiveUserName: {{ directiveUserName }}</div>' +
+            '<div>' +
+            '   <label>Name:</label>' +
+            '   <input ng-model="directiveUserInfo.name">' +
+            '</div>' +
+            '<div>directive.directiveUserInfo: <span class="highlight">{{ directiveUserInfo }}</span></div>' +
             '</div>',
         controller: function ($scope) {
-            console.log('directiveUserName: ' + $scope.directiveUserName)
+            console.log('directiveUserInfo: ' + JSON.stringify($scope.directiveUserInfo))
         }
     }
 })
